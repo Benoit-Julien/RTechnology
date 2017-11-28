c441d348d53df19cfdccc827871d13145f4b80ce
Julien <julien1.benoit@epitech.eu>
2017-11-28 22:56:51 +0100

    Init Project
    Add all CMAke file end all dependencies

---
+++ Raytracer/entry_point.cpp
@@ -0,0 +1,30 @@
+/*
+** entry_point.cpp for RTechnology in
+**
+** Made by julien1.benoit@epitech.eu
+** Login   <julien1.benoit@epitech.eu>
+**
+** Started on  mar. nov. 20:26 2017 benoit_g
+** Last update mar. nov. 20:26 2017 benoit_g
+*/
+
+#include <iostream>
+#include <arrayfire.h>
+
+void entry_point(int device)
+{
+  af::info();
+  char t_device_name[64] = {0};
+  char t_device_platform[64] = {0};
+  char t_device_toolkit[64] = {0};
+  char t_device_compute[64] = {0};
+  af::deviceInfo(t_device_name, t_device_platform, t_device_toolkit, t_device_compute);
+
+  printf("Device name: %s\n", t_device_name);
+  printf("Platform name: %s\n", t_device_platform);
+  printf("Toolkit: %s\n", t_device_toolkit);
+  printf("Compute version: %s\n", t_device_compute);
+
+  af::array undefined_1D(100);
+  std::cout << "Entry Point" << std::endl;
+}
---
+++ Raytracer/include/Raytracer/Raytracer.h
@@ -0,0 +1,16 @@
+/*
+** Raytracer.h for RTechnology in
+**
+** Made by julien1.benoit@epitech.eu
+** Login   <julien1.benoit@epitech.eu>
+**
+** Started on  mar. nov. 20:54 2017 benoit_g
+** Last update mar. nov. 20:54 2017 benoit_g
+*/
+
+#ifndef RTECHNOLOGY_RAYTRACER_H
+#define RTECHNOLOGY_RAYTRACER_H
+
+void entry_point(int device);
+
+#endif /* !RTECHNOLOGY_RAYTRACER_H */
